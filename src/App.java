ca492b3801ed47e123c8964cb46e6ba7e43cc815
sergey <kikimarik@inbox.ru>
2020-12-19 19:56:49 +0300

    4. App.checkFromRange

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 public class App {
+    static final int MAX_RANGE_NUMBER = 30;
+    static final int MIN_RANGE_NUMBER = 10;
+
     public static void main(String[] args) {
         int number = 1;
         double floatNumber = 1.1;
@@ -13,6 +16,15 @@ public class App {
         float third = (float) 1.4;
         float fourth = (float) 1.05;
         System.out.println(App.getSummary(first, second, third, fourth));
+
+        int firstInt = (int) Math.round(Math.random() * App.MAX_RANGE_NUMBER);
+        int secondInt = (int) Math.round(Math.random() * App.MAX_RANGE_NUMBER);
+        System.out.println(App.checkFromRange(firstInt, secondInt));
+    }
+
+    private static boolean checkFromRange(int firstInt, int secondInt) {
+        int sum = firstInt + secondInt;
+        return sum >= App.MIN_RANGE_NUMBER && sum <= App.MAX_RANGE_NUMBER;
     }
 
     private static float getSummary(float a, float b, float c, float d) {
