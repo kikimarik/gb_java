61704bd185feedbb4f3d9777b9c178873dd70d89
sergey <kikimarik@inbox.ru>
2020-12-19 19:20:12 +0300

    2. Init variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
-
+        int number = 1;
+        double floatNumber = 1.1;
+        boolean checkSum = floatNumber + number == 2.2;
+        char a = 'a';
+        String str = "I`m the string";
+        String charString = String.valueOf(a);
+        String[] strArr = { str, str.replaceFirst("the", charString) };
     }
 }
