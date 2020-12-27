14f1b7d2b96ed6b36a45adde694cbe2aa4049fe9
sergey <kikimarik@inbox.ru>
2020-12-28 01:39:52 +0300

    2. Practice.runWordGame overloaded

--- src/Practice.java
+++ src/Practice.java
@@ -1,10 +1,44 @@
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Practice {
-    public static void main(String[] args) throws Exception {
-        Practice.runNumberGame();
+    public static void main(String[] args) {
+
+        String[] words = {
+                "apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot",
+                "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+                "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"
+        };
+        System.out.println(Arrays.toString(words));
+        Practice.runWordGame(words);
+    }
+
+    private static void runWordGame(String[] words) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length - 1);
+        String answer = words[randomIndex];
+        Practice.runWordGame(answer, scanner);
+        scanner.close();
+    }
+
+    private static void runWordGame(String word, Scanner scanner) {
+        System.out.println("Please choice word of array \n");
+        String userAnswer = scanner.next().toLowerCase(Locale.ROOT);
+        if (userAnswer.equals(word)) {
+            System.out.println("You are right. It is really " + word);
+        } else {
+            StringBuilder output = new StringBuilder();
+            int minLength = Math.min(word.length(), userAnswer.length());
+            for (int i = 0; i < minLength; i++) {
+                output.append(word.charAt(i) == userAnswer.charAt(i) ? word.charAt(i) : "#");
+            }
+            output.append("#".repeat(15 - minLength));
+            System.out.println(output.toString());
+            Practice.runWordGame(word, scanner);
+        }
     }
 
     private static void runNumberGame() throws Exception {
