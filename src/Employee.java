2f477a6e18acdf8641896076adfe14a30fcbecca
sergey <kikimarik@inbox.ru>
2021-01-13 22:57:19 +0300

    2. add construct method and call it from another class

--- src/Employee.java
+++ src/Employee.java
@@ -5,4 +5,20 @@ public class Employee {
     private String phone;
     private String salary;
     private int age;
+
+    public Employee(
+            String name,
+            String position,
+            String email,
+            String phone,
+            String salary,
+            int age
+    ) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 }
---
+++ src/EmployeeApp.java
@@ -0,0 +1,12 @@
+public class EmployeeApp {
+    public static void main(String[] args) {
+        Employee employee = new Employee(
+                "Sergey Sorokin",
+                "backend developer",
+                "kikimarik@inbox.ru",
+                "+79216479264",
+                "100000 rubles",
+                25
+        );
+    }
+}
