f028804e1639c074e6a7f01b3c64af2878ad0674
sergey <kikimarik@inbox.ru>
2021-01-13 22:54:32 +0300

    1. Class Employee

---
+++ src/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private String salary;
+    private int age;
+}
