1778daa9516d73a2bc7af5c612fa3ea9f5c15a1a
sergey <kikimarik@inbox.ru>
2021-01-23 12:40:09 +0300

    6. add plate.addFood()

--- src/CatPlateUsage.java
+++ src/CatPlateUsage.java
@@ -3,17 +3,18 @@ import catplate.Plate;
 
 public class CatPlateUsage {
     public static void main(String[] args) {
-        Plate plate = new Plate(10);
+        Plate plate = new Plate(0);
         Cat[] cats = {
                 new Cat("Tom", 2), // done
                 new Cat("Molly", 1), // done
-                new Cat("Fat guy", 6), // done
-                new Cat("Augustin", 3), // fail
-                new Cat("Dr. Bob", 2), // fail
+                new Cat("Fat guy", 6), // fail
+                new Cat("Augustin", 3), // done
+                new Cat("Dr. Bob", 2), // done
                 new Cat("Lizzy", 1), // done
-                new Cat("Stacy", 2), // fail
+                new Cat("Stacy", 2), // done
         };
         for (Cat cat : cats) {
+            plate.addFood(2); // add 2 food units
             cat.eat(plate);
             CatPlateUsage.checkHungry(cat);
         }
